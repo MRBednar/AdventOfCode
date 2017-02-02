91a07d0f1b146344faca24ca202754b090e38a64
MR Bednar <M.R.Bednar@gmail.com>
2017-02-01 20:51:08 -0800

    Day3 Part 2 added

--- AdventOfCodeRunner/Day3.cs
+++ AdventOfCodeRunner/Day3.cs
@@ -9,10 +9,15 @@ namespace AdventOfCodeRunner
     {
         public void Run()
         {
-            int validTriangles = 0;
+            int part1Triangles = 0;
+            int part2Triangles = 0;
             int side1;
             int side2;
             int side3;
+            int[] triangle1 = new int[3];
+            int[] triangle2 = new int[3];
+            int[] triangle3 = new int[3];
+            int row = 0;
 
             using (StreamReader sr = new StreamReader("Day3Input.txt"))
             {
@@ -22,16 +27,37 @@ namespace AdventOfCodeRunner
                     string[] inputSplit = {" "};
                     string[] args = input.Split(inputSplit, StringSplitOptions.RemoveEmptyEntries);
                     side1 = int.Parse(args[0]);
+                    triangle1[row] = side1;
                     side2 = int.Parse(args[1]);
+                    triangle2[row] = side2;
                     side3 = int.Parse(args[2]);
+                    triangle3[row] = side3;
                     if (side1+side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2)
                     {
-                        validTriangles++;
+                        part1Triangles++;
+                    }
+                    row++;
+                    if (row % 3 == 0)
+                    {
+                        if (triangle1[0] + triangle1[1] > triangle1[2] && triangle1[1] + triangle1[2] > triangle1[0] && triangle1[2] + triangle1[0] > triangle1[1])
+                        {
+                            part2Triangles++;
+                        }
+                        if (triangle2[0] + triangle2[1] > triangle2[2] && triangle2[1] + triangle2[2] > triangle2[0] && triangle2[2] + triangle2[0] > triangle2[1])
+                        {
+                            part2Triangles++;
+                        }
+                        if (triangle3[0] + triangle3[1] > triangle3[2] && triangle3[1] + triangle3[2] > triangle3[0] && triangle3[2] + triangle3[0] > triangle3[1])
+                        {
+                            part2Triangles++;
+                        }
+                        row = 0;
                     }
                 }
 
             }
-            Console.WriteLine("Number of valid Triangles: {0}", validTriangles);
+            Console.WriteLine("Number of valid Triangles From Part 1: {0}", part1Triangles);
+            Console.WriteLine("Number of valid Triangles From Part 2: {0}", part2Triangles);
         }
     }
 }
