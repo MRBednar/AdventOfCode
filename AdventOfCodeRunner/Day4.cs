0239eba62583e0f5c58ef5f1f15eba29ce76df5e
mbednar <mbednar@us.imshealth.com>
2017-05-01 20:55:09 -0700

    Some Day 4 Work

--- AdventOfCodeRunner/Day4.cs
+++ AdventOfCodeRunner/Day4.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCodeRunner
 {
@@ -9,11 +11,12 @@ namespace AdventOfCodeRunner
         public void Run()
         {
             // For keeping track of the ID Sum total
-            int sectorIDSum = 0;
+            int sectorIDSum1 = 0;
             string numberSplit = @"\d";
             string letterSplit = @"\D";
             string cheksumSplit = @"\[";
             string bracktRMV = @"\]";
+
             // remove the hyphens because they don't mean anything (Part 1)
             string nohyphen;
             // Regex functions for cleaning up the inputs (Part 1)
@@ -30,8 +33,45 @@ namespace AdventOfCodeRunner
                     string[] letterAndCheck = Regex.Split(letters, cheksumSplit);
                     letterAndCheck[1] = removeBrackets.Replace(letterAndCheck[1], String.Empty);
                     string numbers = numberRgx.Replace(nohyphen, String.Empty);
-                    int test = letters.Length;
+                    char[] distinctletters = letterAndCheck[0].Distinct().ToArray();
+                    int checkInt = 100000000;
+                    char checkChar = 'A';
+                    bool isGood = true;
+                    Console.WriteLine("Checking Line: {0}", input);
+                    foreach (char letter in letterAndCheck[1])
+                    {
+                        int count = letterAndCheck[0].Count(f => f == letter);
+                        Console.WriteLine("Letter: {0}   | Count: {1}", letter, count);
+                        if (count > checkInt)
+                        {
+                            //Console.WriteLine("Invalid letter: {0}", letter);
+                            isGood = false;
+                            break;
+                        }
+                        else if (count == checkInt)
+                        {
+                            int index = char.ToUpper(letter) - 64;
+                            int checkIndex = char.ToUpper(checkChar) - 64;
+                            if (index < checkIndex)
+                            {
+                                //Console.WriteLine("Invalid letter: {0}", letter);
+                                isGood = false;
+                                break;
+                            }
+                        }
+                        checkInt = count;
+                        checkChar = letter;
+                    }
+
+                    if (isGood)
+                    {
+                        sectorIDSum1 = sectorIDSum1 + int.Parse(numbers);
+                        Console.WriteLine("Valid line, Current SectorID Sum: {0}", sectorIDSum1);
+                    }
+
+                    Console.ReadKey();
                 }
+                Console.WriteLine("Sum of Sector IDs: {0}", sectorIDSum1);
             }
         }
     }
