3c695d630f0c5b1d909ec5dd56b9ba285650d581
mbednar <mbednar@us.imshealth.com>
2017-05-03 17:04:57 -0700

    Day 5 Part 1 solution

--- AdventOfCodeRunner/Day5.cs
+++ AdventOfCodeRunner/Day5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace AdventOfCodeRunner
 {
@@ -10,6 +11,46 @@ namespace AdventOfCodeRunner
     {
         public void Run()
         {
+            string password = "";
+            int i = 0;
+            Console.WriteLine(password.Length);
+            while (password.Length < 8)
+            {
+                string hash = CalculateMD5Hash("ugkcyxxp" + i);
+                if (hash.StartsWith("00000"))
+                {
+                    password += hash[5];
+                }
+                i++;
+            }
+            Console.WriteLine(password);
+        }
+
+        public string CalculateMD5Hash(string input)
+
+        {
+
+            // step 1, calculate MD5 hash from input
+
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+
+            {
+
+                sb.Append(hash[i].ToString("x2"));
+
+            }
+
+            return sb.ToString();
 
         }
     }
